b586c6bf2ab3afb0916e8744347d130d575e939e
Rennan Nogueira <rmelo.nogueira123@gmail.com>
2022-09-13 10:21:27 -0300

    project done

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,29 +1,45 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
-
-import entities.Residence;
 
 public class Program {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		List<Integer> list = new ArrayList<>();
+		List<Integer> tested = new ArrayList<>();
+		List<Integer> apaixornados = new ArrayList<>();
+		Integer testing;
+		int cont =0;
 		int n = sc.nextInt();
-		Integer c = 0;
 		sc.nextLine();
-
-		// while for each city
 		while (n != 0) {
-			c++;
-
+			String line = sc.nextLine();
+			String[] fields = line.split(" ");
 			// for to each residence
 			for (int i = 0; i < n; i++) {
-				String line = sc.nextLine();
-				String[] fields = line.split(" ");
-				int x = Integer.parseInt(fields[0]);
-				int y = Integer.parseInt(fields[1]);
-				int z = (int) Math.floor(y / x);
-				list.add(new Residence(x, y, z));
-				map.put(c, list);
+				list.add(Integer.parseInt(fields[i]));
 			}
+			for (int i = 0; i < n; i++) {
+				testing = list.get(i);
+				if(!tested.contains(testing)) {
+					cont=0;
+					for (int i1 = 0; i1 < n; i1++) {
+						if (list.get(i1)==testing) {
+							cont++;
+						}
+					}
+					if (cont%2!=0) {
+						apaixornados.add(testing);
+					}
+				}
+				tested.add(list.get(i));
+			}
+			n = sc.nextInt();
+			sc.nextLine();
+		}
+		System.out.println(Arrays.toString(apaixornados.toArray()));
 	}
 }
